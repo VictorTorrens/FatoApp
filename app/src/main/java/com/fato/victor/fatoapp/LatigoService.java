3f9dd6b43a84df171160eb5a97d0a6ee14fecc15
Victor <victor.torrens@borealos.com>
2018-12-31 13:36:37 +0100

    redesign

---
+++ app/src/main/java/com/fato/victor/fatoapp/LatigoService.java
@@ -0,0 +1,13 @@
+package com.fato.victor.fatoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LatigoService extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_latigo_service);
+    }
+}
